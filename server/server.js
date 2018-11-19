3a1af5323699bdd97c22b634d9e6b39460f99091
stoosmith <stoosmith@yahoo.co.uk>
2018-11-19 12:21:57 +0000

    first-commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('this is working');
+})
+
+const port = process.env.PORT || 3001;
+app.listen(port, ()=>{
+    console.log('SERVER RUNNING ON PORT: ' + port)
+})
+
+/*route thoughts
+
+/ - res = this is working
+/signin - Send(req):POST = Return(res):success/fail
+/register - req:POST = res:user
+/user/:userid -req:GET = res:user
+
+
+*/
